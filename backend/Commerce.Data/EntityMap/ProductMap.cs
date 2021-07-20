cb81c55387aaaf45370f6205537f23e94692fda2
Anderson Freitas <anderson.santos@mag-it.com.br>
2021-07-20 14:07:27 -0300

    migration de produtos inserindo dados iniciais

--- backend/Commerce.Data/EntityMap/ProductMap.cs
+++ backend/Commerce.Data/EntityMap/ProductMap.cs
@@ -1,6 +1,7 @@
 ﻿using Commerce.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 
 namespace Commerce.Data.EntityMap
 {
@@ -10,6 +11,43 @@ namespace Commerce.Data.EntityMap
         {
             builder.ToTable("Product");
             builder.HasKey(x => x.Id);
+            builder.HasData(new Product[] {
+                new Product {
+                    Id = Guid.NewGuid(),
+                    Name = "Produto Teste 1",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    PhotoUri = "https://picsum.photos/seed/1/200/200",
+                    Price = 10.99F,
+                },
+                new Product {
+                    Id = Guid.NewGuid(),
+                    Name = "Produto Teste 2",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    PhotoUri = "https://picsum.photos/seed/2/200/200",
+                    Price = 15.99F,
+                },
+                new Product {
+                    Id = Guid.NewGuid(),
+                    Name = "Produto Teste 3",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    PhotoUri = "https://picsum.photos/seed/3/200/200",
+                    Price = 599.99F,
+                },
+                new Product {
+                    Id = Guid.NewGuid(),
+                    Name = "Produto Teste 4",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    PhotoUri = "https://picsum.photos/seed/4/200/200",
+                    Price = 599.99F,
+                },
+                new Product {
+                    Id = Guid.NewGuid(),
+                    Name = "Produto Teste 5",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    PhotoUri = "https://picsum.photos/seed/5/200/200",
+                    Price = 599.99F,
+                },
+            });
         }
     }
 }
---
+++ backend/Commerce.Data/Migrations/20210720170242_DataSeedingProducts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Commerce.Data.Migrations
+{
+    public partial class DataSeedingProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "CascadeMode", "Description", "Name", "PhotoUri", "Price" },
+                values: new object[] { new Guid("449fed22-3db3-47a9-a564-03f15c8e5e61"), 0, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Produto Teste 1", "https://picsum.photos/seed/1/200/200", 10.989999771118164 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "CascadeMode", "Description", "Name", "PhotoUri", "Price" },
+                values: new object[] { new Guid("a3d35592-7090-47a7-8841-7a423c319e2d"), 0, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Produto Teste 2", "https://picsum.photos/seed/2/200/200", 15.989999771118164 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "CascadeMode", "Description", "Name", "PhotoUri", "Price" },
+                values: new object[] { new Guid("238476e4-323e-4eb2-9032-96f15e63be78"), 0, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Produto Teste 3", "https://picsum.photos/seed/3/200/200", 599.989990234375 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "CascadeMode", "Description", "Name", "PhotoUri", "Price" },
+                values: new object[] { new Guid("c5635503-ebec-4fd9-9d00-c816cfae215a"), 0, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Produto Teste 4", "https://picsum.photos/seed/4/200/200", 599.989990234375 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "CascadeMode", "Description", "Name", "PhotoUri", "Price" },
+                values: new object[] { new Guid("9c51a7c1-a189-44bb-8b22-4d74999dc43b"), 0, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Produto Teste 5", "https://picsum.photos/seed/5/200/200", 599.989990234375 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: new Guid("238476e4-323e-4eb2-9032-96f15e63be78"));
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: new Guid("449fed22-3db3-47a9-a564-03f15c8e5e61"));
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: new Guid("9c51a7c1-a189-44bb-8b22-4d74999dc43b"));
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: new Guid("a3d35592-7090-47a7-8841-7a423c319e2d"));
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: new Guid("c5635503-ebec-4fd9-9d00-c816cfae215a"));
+        }
+    }
+}
