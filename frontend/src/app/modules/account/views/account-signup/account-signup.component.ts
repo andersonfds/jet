9da26881464404a6f5dbca99bf89d267ef5d07a6
Anderson Freitas <anderson.santos@mag-it.com.br>
2021-07-20 13:36:58 -0300

    navegação dinâmica na tela de login

--- frontend/src/app/modules/account/views/account-signup/account-signup.component.ts
+++ frontend/src/app/modules/account/views/account-signup/account-signup.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { plainToClass } from 'class-transformer';
 import { ErrorResponse } from 'src/app/shared/models/error.interface';
 import { AccountService } from '../../services/account.service';
@@ -13,6 +14,7 @@ import { AccountSignupRequestDto } from '../models/account-signup.request.dto';
 })
 export class AccountSignupComponent implements OnInit {
 
+  nextRoute: string = '/';
   signupForm?: FormGroup;
   passwordVisible: boolean = false;
   errorMessage?: string;
@@ -21,9 +23,12 @@ export class AccountSignupComponent implements OnInit {
   constructor(
     private readonly fb: FormBuilder,
     private readonly _accountService: AccountService,
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _router: Router,
   ) { }
 
   ngOnInit(): void {
+    this._activatedRoute.queryParamMap.subscribe((a) => this.switchNextRoute(a.get('next')));
     this.signupForm = this.fb.group({
       name: [null, Validators.compose([Validators.required])],
       email: [null, Validators.compose([Validators.required, Validators.email])],
@@ -53,6 +58,12 @@ export class AccountSignupComponent implements OnInit {
 
   private redirect() {
     this.isLoading = false;
+    this._router.navigate([this.nextRoute || '/']);
+  }
+
+  private switchNextRoute(nextRoute: string | null) {
+    if (nextRoute != null)
+      this.nextRoute = nextRoute;
   }
 
   get hasError(): boolean {
